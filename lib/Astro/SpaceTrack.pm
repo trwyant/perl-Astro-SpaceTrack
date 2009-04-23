e3415e0098237c070a66ba20d0b4c5c622af391b
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2009-04-23 01:09:21 +0000

    Production version
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-SpaceTrack/trunk@1377 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -90,7 +90,7 @@ use warnings;
 
 use base qw{Exporter};
 
-our $VERSION = '0.039_01';
+our $VERSION = '0.040';
 our @EXPORT_OK = qw{shell BODY_STATUS_IS_OPERATIONAL BODY_STATUS_IS_SPARE
     BODY_STATUS_IS_TUMBLING};
 our %EXPORT_TAGS = (
