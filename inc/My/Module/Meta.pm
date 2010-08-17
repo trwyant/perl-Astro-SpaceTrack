866e4fc01eac22294e3f9006162add129384e9ec
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2010-08-17 16:29:18 +0000

    Centralize dependencies into inc/My/Module/Meta.pm.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-SpaceTrack/trunk@1926 f6fd3633-1913-47c2-bad4-372d75c948ec

---
+++ inc/My/Module/Meta.pm
@@ -0,0 +1,144 @@
+package My::Module::Meta;
+
+use 5.006;
+
+use strict;
+use warnings;
+
+use Carp;
+
+sub new {
+    my ( $class ) = @_;
+    ref $class and $class = ref $class;
+    my $self = {
+	distribution => $ENV{MAKING_MODULE_DISTRIBUTION},
+    };
+    bless $self, $class;
+    return $self;
+}
+
+sub distribution {
+    my ( $self ) = @_;
+    return $self->{distribution};
+}
+
+sub requires {
+    my ( $self, @extra ) = @_;
+
+    return {
+	'Compress::Zlib' => 0,
+	'HTML::Parser' => 0,
+	'HTTP::Response' => 0,
+	'HTTP::Status' => 0,
+	'LWP::UserAgent' => 0,
+	'Params::Util' => 0.12,
+	@extra,
+    };
+}
+
+sub requires_perl {
+    return 5.006;
+}
+
+
+1;
+
+__END__
+
+=head1 NAME
+
+My::Module::Meta - Information needed to build Astro::SpaceTrack
+
+=head1 SYNOPSIS
+
+ use lib qw{ inc };
+ use My::Module::Meta;
+ my $meta = My::Module::Meta->new();
+ use YAML;
+ print "Required modules:\n", Dump(
+     $meta->requires() );
+
+=head1 DETAILS
+
+This module centralizes information needed to build C<App::Satpass2>. It
+is private to the C<App::Satpass2> package, and may be changed or
+retracted without notice.
+
+=head1 METHODS
+
+This class supports the following public methods:
+
+=head2 new
+
+ use lib qw{ inc };
+ my $meta = My::Module::Meta->new();
+
+This method instantiates the class.
+
+=head2 distribution
+
+ if ( $meta->distribution() ) {
+     print "Making distribution\n";
+ } else {
+     print "Not making distribution\n";
+ }
+
+This method returns the value of the environment variable
+C<MAKING_MODULE_DISTRIBUTION> at the time the object was instantiated.
+
+=head2 requires
+
+ use YAML;
+ print Dump( $meta->requires() );
+
+This method computes and returns a reference to a hash describing
+the modules required to run the C<App::Satpass2> package, suitable for
+use in a F<Build.PL> C<requires> key, or a F<Makefile.PL> C<PREREQ_PM>
+key. Any additional arguments will be appended to the generated hash. In
+addition, unless L<distribution()|/distribution> is true,
+configuration-specific modules may be added.
+
+=head2 requires_perl
+
+ print 'This package requires Perl ', $meta->requires_perl(), "\n";
+
+This method returns the version of Perl required by the package.
+
+=head1 ATTRIBUTES
+
+This class has no public attributes.
+
+
+=head1 ENVIRONMENT
+
+=head2 MAKING_MODULE_DISTRIBUTION
+
+This environment variable should be set to a true value if you are
+making a distribution. This ensures that no configuration-specific
+information makes it into F<META.yml>.
+
+
+=head1 SUPPORT
+
+Support is by the author. Please file bug reports at
+L<http://rt.cpan.org>, or in electronic mail to the author.
+
+=head1 AUTHOR
+
+Thomas R. Wyant, III F<wyant at cpan dot org>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2010, Thomas R. Wyant, III
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl 5.10.0. For more details, see the full text
+of the licenses in the directory LICENSES.
+
+This program is distributed in the hope that it will be useful, but
+without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+=cut
+
+# ex: set textwidth=72 :
