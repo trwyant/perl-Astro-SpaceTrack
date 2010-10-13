b8997e6c1a21bb2d62eaef2f047e3ea947bd1756
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2010-10-13 03:08:01 +0000

    Update version to 0.050. Update Changes for this version.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-SpaceTrack/trunk@2056 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -90,7 +90,7 @@ use warnings;
 
 use base qw{Exporter};
 
-our $VERSION = '0.049_01';
+our $VERSION = '0.050';
 our @EXPORT_OK = qw{shell BODY_STATUS_IS_OPERATIONAL BODY_STATUS_IS_SPARE
     BODY_STATUS_IS_TUMBLING};
 our %EXPORT_TAGS = (
--- lib/Astro/SpaceTrack/Parser.pm
+++ lib/Astro/SpaceTrack/Parser.pm
@@ -25,7 +25,7 @@ use base qw{HTML::Parser};
 
 use Carp;
 
-our $VERSION = '0.049_01';
+our $VERSION = '0.050';
 
 my %target = (
     table => {
