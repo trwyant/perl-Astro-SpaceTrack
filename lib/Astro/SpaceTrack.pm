e6c7a6227b6ab474c8ca523a901f5c5bdd457f12
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2011-02-11 01:02:26 +0000

    Version to 0.051. Document changes since 0.050.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-SpaceTrack/trunk@2155 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -90,7 +90,7 @@ use warnings;
 
 use base qw{Exporter};
 
-our $VERSION = '0.050';
+our $VERSION = '0.051';
 our @EXPORT_OK = qw{shell BODY_STATUS_IS_OPERATIONAL BODY_STATUS_IS_SPARE
     BODY_STATUS_IS_TUMBLING};
 our %EXPORT_TAGS = (
