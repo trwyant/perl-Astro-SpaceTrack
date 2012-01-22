b6c2a577cd034e20344a8876733ed11ee565ece0
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2012-01-22 16:07:01 +0000

    Update copyright to 2012.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-SpaceTrack/trunk@3019 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -3563,7 +3563,7 @@ Thomas R. Wyant, III (F<wyant at cpan dot org>)
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2005-2011 by Thomas R. Wyant, III (F<wyant at cpan dot org>).
+Copyright 2005-2012 by Thomas R. Wyant, III (F<wyant at cpan dot org>).
 
 =head1 LICENSE
 
