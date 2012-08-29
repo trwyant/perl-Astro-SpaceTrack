ff75982c8764106f832f03863ef6915cf0e6f86d
tom <tom@f6fd3633-1913-47c2-bad4-372d75c948ec>
2012-08-29 15:15:20 +0000

    Bump version to 0.063. Update Changes file for changes since 0.062.
    git-svn-id: file:///Users/tom/Code/svn/repos/Astro-SpaceTrack/trunk@3464 f6fd3633-1913-47c2-bad4-372d75c948ec

--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -201,7 +201,7 @@ use warnings;
 
 use base qw{Exporter};
 
-our $VERSION = '0.062';
+our $VERSION = '0.063';
 our @EXPORT_OK = qw{shell BODY_STATUS_IS_OPERATIONAL BODY_STATUS_IS_SPARE
     BODY_STATUS_IS_TUMBLING};
 our %EXPORT_TAGS = (
