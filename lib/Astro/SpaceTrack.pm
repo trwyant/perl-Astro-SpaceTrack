91e7202b55f2079d3aa4133d7c580340a0ee0d74
Tom Wyant <wyant@cpan.org>
2014-04-14 18:46:30 -0400

    Version 0.085_01
    Set version to 0.085_01. Update Changes file for changes since 0.085.

--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -109,7 +109,7 @@ use warnings;
 
 use base qw{Exporter};
 
-our $VERSION = '0.085';
+our $VERSION = '0.085_01';
 our @EXPORT_OK = qw{shell BODY_STATUS_IS_OPERATIONAL BODY_STATUS_IS_SPARE
     BODY_STATUS_IS_TUMBLING};
 our %EXPORT_TAGS = (
