e90198212d8fe8a7f880ffa90b8195a0aad58ce6
Tom Wyant <wyant@cpan.org>
2014-12-28 16:21:09 -0500

    Version 0.088_04.
    Set version to 0.088_04. Update Changes file for changes since 0.088_03.

--- inc/Mock/LWP/UserAgent.pm
+++ inc/Mock/LWP/UserAgent.pm
@@ -13,7 +13,7 @@ use HTTP::Response;
 use LWP::UserAgent;
 use JSON;
 
-our $VERSION = '0.088_03';
+our $VERSION = '0.088_04';
 
 our $CANNED_RESPONSE_DIR = File::Spec->catdir(
     qw{ t data Mock-LWP-UserAgent } );
