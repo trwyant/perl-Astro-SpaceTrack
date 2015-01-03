306ddc373d2a0c16da92b575a225cdf4d470b085
Tom Wyant <wyant@cpan.org>
2015-01-03 07:12:27 -0500

    Update copyright to 2015.

--- inc/My/Module/Meta.pm
+++ inc/My/Module/Meta.pm
@@ -214,7 +214,7 @@ Thomas R. Wyant, III F<wyant at cpan dot org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010-2014 by Thomas R. Wyant, III
+Copyright (C) 2010-2015 by Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
