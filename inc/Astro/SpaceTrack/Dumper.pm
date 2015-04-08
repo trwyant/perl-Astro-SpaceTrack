d154d8f4da92df0553c6b428ba8149becc60845c
Tom Wyant <wyant@cpan.org>
2015-04-08 17:55:15 -0400

    Version 0.090_03
    Set version to 0.090_03. Update Changes file for changes since 0.090_02.

--- inc/Astro/SpaceTrack/Dumper.pm
+++ inc/Astro/SpaceTrack/Dumper.pm
@@ -18,7 +18,7 @@ eval {
     1;
 } or croak 'Can not load Mock::LWP::UserAgent. Code must be run from the base directory of the Astro-SpaceTrack distribution';
 
-our $VERSION = '0.090_02';
+our $VERSION = '0.090_03';
 
 {
     my $json;
--- inc/Mock/LWP/UserAgent.pm
+++ inc/Mock/LWP/UserAgent.pm
@@ -13,7 +13,7 @@ use HTTP::Response;
 use LWP::UserAgent;
 use JSON;
 
-our $VERSION = '0.090_02';
+our $VERSION = '0.090_03';
 
 our $CANNED_RESPONSE_DIR = File::Spec->catdir(
     qw{ t data Mock-LWP-UserAgent } );
