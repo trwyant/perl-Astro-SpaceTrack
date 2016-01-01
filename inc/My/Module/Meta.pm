e6dbd301ea01e41bb3b8924464e6ff5274c67253
Tom Wyant <wyant@cpan.org>
2016-01-01 14:49:01 -0500

    Add GitHub repository to metadata.

--- inc/My/Module/Meta.pm
+++ inc/My/Module/Meta.pm
@@ -31,6 +31,27 @@ sub distribution {
     return $self->{distribution};
 }
 
+sub meta_merge {
+    return {
+	'meta-spec'	=> {
+	    version	=> 2,
+	},
+	dynamic_config	=> 1,
+	no_index	=> {
+	    directory	=> [ qw{ inc t tools xt } ],
+	},
+	resources	=> {
+	    bugtracker	=> 'https://rt.cpan.org/Public/Dist/Display.html?Name=Astro-SpaceTrack',
+	    license	=> 'http://dev.perl.org/licenses/',
+	    repository	=> {
+		type	=> 'git',
+		url	=> 'git://github.com/trwyant/perl-Astro-SpaceTrack.git',
+		web	=> 'https://github.com/trwyant/perl-Astro-SpaceTrack',
+	    },
+	}
+    };
+}
+
 sub notice {
     my ( $self, $opt, $prompter ) = @_;
 
@@ -157,6 +178,16 @@ C<< {META_MERGE}->{build_requires} >> key.
 
 This method returns the value of the environment variable
 C<MAKING_MODULE_DISTRIBUTION> at the time the object was instantiated.
+
+=head2 meta_merge
+
+ use YAML;
+ print Dump( $meta->meta_merge() );
+
+This method returns a reference to a hash describing the meta-data which
+has to be provided by making use of the builder's C<meta_merge>
+functionality. This includes the C<dynamic_config>, C<no_index> and
+C<resources> data.
 
 =head2 notice
 
