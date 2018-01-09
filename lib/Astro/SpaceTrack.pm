74c73dad50c96e21ab29dc406c1cc9e18abc8467
Tom Wyant <wyant@cpan.org>
2018-01-09 10:50:47 -0500

    Update copyright to 2018.

--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -6720,7 +6720,7 @@ Thomas R. Wyant, III (F<wyant at cpan dot org>)
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2005-2017 by Thomas R. Wyant, III (F<wyant at cpan dot org>).
+Copyright 2005-2018 by Thomas R. Wyant, III (F<wyant at cpan dot org>).
 
 =head1 LICENSE
 
