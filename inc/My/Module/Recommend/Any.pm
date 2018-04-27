9aaf139c56fb9d38485b196d0604be01433898c7
Tom Wyant <wyant@cpan.org>
2018-04-27 08:52:11 -0400

    Version 0.107
    Set version to 0.107. Update Changes file for changes since v00.106_01.

--- inc/My/Module/Recommend/Any.pm
+++ inc/My/Module/Recommend/Any.pm
@@ -8,7 +8,7 @@ use Exporter;
 
 our @ISA = qw{ Exporter };
 
-our $VERSION = '0.106_01';
+our $VERSION = '0.107';
 
 our @EXPORT_OK = qw{ __any };
 
--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -128,7 +128,7 @@ use Exporter;
 
 our @ISA = qw{ Exporter };
 
-our $VERSION = '0.106_01';
+our $VERSION = '0.107';
 our @EXPORT_OK = qw{
     shell
 
