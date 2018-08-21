66d49844f939ff4a180a810acb8b15eaa0663a95
Tom Wyant <wyant@cpan.org>
2018-08-21 10:00:48 -0400

    Version 0.115_01
    Set version to 0.115_01. Update Changes file for changes since v00.115.

--- inc/My/Module/Recommend/Any.pm
+++ inc/My/Module/Recommend/Any.pm
@@ -8,7 +8,7 @@ use Exporter;
 
 our @ISA = qw{ Exporter };
 
-our $VERSION = '0.115';
+our $VERSION = '0.115_01';
 
 our @EXPORT_OK = qw{ __any };
 
--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -128,7 +128,7 @@ use Exporter;
 
 our @ISA = qw{ Exporter };
 
-our $VERSION = '0.115';
+our $VERSION = '0.115_01';
 our @EXPORT_OK = qw{
     shell
 
