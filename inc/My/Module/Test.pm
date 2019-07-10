35aade17bc671ab63222cf663b77615a2f32216a
Tom Wyant <wyant@cpan.org>
2019-07-10 07:11:18 -0400

    Correct McCants' web site check for tests.
    This was occasioned by his Iridium Status page (which I was looking for
    to see if his site was live) going away.

--- inc/My/Module/Test.pm
+++ inc/My/Module/Test.pm
@@ -195,7 +195,7 @@ sub not_defined ($$) {	## no critic (ProhibitSubroutinePrototypes)
 		url	=> 'http://celestrak.com/',
 	    },
 	    'mike.mccants'	=> {
-		url	=> 'http://www.prismnet.com/~mmccants/tles/iridium.html',
+		url	=> 'http://www.prismnet.com/~mmccants/',
 	    },
 	    'rod.sladen'	=> {
 		url	=> 'http://www.rod.sladen.org.uk/iridium.htm',
