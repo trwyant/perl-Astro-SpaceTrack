21eadc5a96f794fe244e997efd23ff034789ed81
Tom Wyant <wyant@cpan.org>
2020-08-17 08:00:47 -0400

    Version 0.141
    Set version to 0.141. Update Changes file for changes since v00.140_01.

--- inc/My/Module/Recommend/Any.pm
+++ inc/My/Module/Recommend/Any.pm
@@ -8,7 +8,7 @@ use Exporter;
 
 our @ISA = qw{ Exporter };
 
-our $VERSION = '0.140_01';
+our $VERSION = '0.141';
 
 our @EXPORT_OK = qw{ __any };
 
--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -106,7 +106,7 @@ use Exporter;
 
 our @ISA = qw{ Exporter };
 
-our $VERSION = '0.140_01';
+our $VERSION = '0.141';
 our @EXPORT_OK = qw{
     shell
 
