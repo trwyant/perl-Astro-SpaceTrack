fca2604a6a1a5cbffd66d06fd615e2a319ee9bc4
Tom Wyant <wyant@cpan.org>
2021-02-16 11:31:18 -0500

    Version 0.143_01
    Set version to 0.143_01. Update Changes file for changes since v00.143.

--- inc/My/Module/Recommend/Any.pm
+++ inc/My/Module/Recommend/Any.pm
@@ -8,7 +8,7 @@ use Exporter;
 
 our @ISA = qw{ Exporter };
 
-our $VERSION = '0.143';
+our $VERSION = '0.143_01';
 
 our @EXPORT_OK = qw{ __any };
 
--- lib/Astro/SpaceTrack.pm
+++ lib/Astro/SpaceTrack.pm
@@ -102,7 +102,7 @@ use Exporter;
 
 our @ISA = qw{ Exporter };
 
-our $VERSION = '0.143';
+our $VERSION = '0.143_01';
 our @EXPORT_OK = qw{
     shell
 
