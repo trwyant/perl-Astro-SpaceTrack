063106d8b5c5fd49f309c7756c5b801b5b3909b8
Tom Wyant <wyant@cpan.org>
2024-06-16 09:18:39 -0400

    Version 0.166
    Set version to 0.166. Update Changes file for changes since v00.165_02.

--- inc/Astro/SpaceTrack/Dumper.pm
+++ inc/Astro/SpaceTrack/Dumper.pm
@@ -19,7 +19,7 @@ our @ISA = qw{ Astro::SpaceTrack };
 use Mock::LWP::UserAgent;
 use My::Module::Test;
 
-our $VERSION = '0.165_02';
+our $VERSION = '0.166';
 
 sub new {
     my ( $class, %arg ) = @_;
