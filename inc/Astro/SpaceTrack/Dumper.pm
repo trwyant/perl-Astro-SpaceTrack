c1a5cdc684ae5f2ea295f44ee3583eeb2a072364
Tom Wyant <wyant@cpan.org>
2024-09-06 11:58:00 -0400

    Version 0.166_02
    Set version to 0.166_02. Update Changes file for changes since v00.166_01.

--- inc/Astro/SpaceTrack/Dumper.pm
+++ inc/Astro/SpaceTrack/Dumper.pm
@@ -19,7 +19,7 @@ our @ISA = qw{ Astro::SpaceTrack };
 use Mock::LWP::UserAgent;
 use My::Module::Test;
 
-our $VERSION = '0.166_01';
+our $VERSION = '0.166_02';
 
 sub new {
     my ( $class, %arg ) = @_;
--- inc/Mock/LWP/UserAgent.pm
+++ inc/Mock/LWP/UserAgent.pm
@@ -13,7 +13,7 @@ use HTTP::Response;
 use LWP::UserAgent;
 use JSON;
 
-our $VERSION = '0.166_01';
+our $VERSION = '0.166_02';
 
 our $CANNED_RESPONSE_FILE;
 
